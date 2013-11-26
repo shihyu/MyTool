ec8ab37b032d5e95c1f02aaa8e7ab578b93426ef
jason_yao <jason_yao@htc.com>
2013-11-27 01:04:29 +0800

    update

---
+++ SCons/Guard.cpp
@@ -0,0 +1,17 @@
+#include "Guard.h"
+#include <iostream>
+
+namespace adapter {
+
+    Guard::Guard(std::string name) {
+        this->setName(name);
+    }
+
+    void Guard::attrack() {
+        std::cout << this->getName() + std::string(" attrack") << std::endl;
+    }
+
+    void Guard::defence() {
+        std::cout << this->getName() + std::string(" defence") << std::endl;
+    }
+}
